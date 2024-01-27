5699620ae293095315b9b2da40daf9bcccce872e
maksim <suslovsms@yandex.ru>
2024-01-27 17:59:01 +0300

    First step

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 token = "6827408238:AAGGauvH6NG8aGqvaNDODiizvOiwfp2TbzQ"
 bot = telebot.TeleBot(token)
@@ -7,7 +8,35 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Привет ✌️ ")
-    bot.infinity_polling()
 
 
-# start_message("Привет")
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    item2 = types.KeyboardButton("Кнопка 2")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+
+
+# @bot.message_handler(content_types=['text'])
+# def message_reply(message):
+#     if message.text == "Кнопка":
+#         bot.send_message(message.chat.id, "https://habr.com/ru/users/lubaznatel/")
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == "Кнопка":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Кнопка 2")
+        markup.add(item1)
+        bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+    elif message.text == "Кнопка 2":
+        bot.send_message(message.chat.id, 'Спасибо за прочтение статьи!')
+
+
+bot.infinity_polling()
+
+
